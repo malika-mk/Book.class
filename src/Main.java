fd9cea3591d4827261fb458ee57de893843b90cd
Malika <malikamkkambay@gmail.com>
2024-09-20 16:53:04 +0600

    The names have been changed

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Book book = new Book("Memoirs of a Geisha", "Arthur Golden", 184, 25);
         book.dispayInfo();
-        Book book2 = new Book("wrt", "e", 34, 45);
+        Book book2 = new Book("The Little Prince", "Antoine Maries Jean-Baptiste ", 190, 30);
         book2.dispayInfo();
-        Book book3 = new Book("dfadf", "srg", 345, 45);
+        Book book3 = new Book("Gossip Girl", "Cecily Brooke von Ziegesar", 345, 45);
         book3.dispayInfo();
 
     }
